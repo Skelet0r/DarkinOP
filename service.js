e04981459b651698547bf99ddd857f7a3700c4ac
Victor Arango <victor.arango@trustvesta.com>
2019-02-21 13:14:02 -0600

    Changes for headers.

--- service.js
+++ service.js
@@ -12,4 +12,65 @@ angular.module('MyApp')
 			web
 		);
     }
+	
+	this.getSummonerData2 = function ()
+    {
+		/*return $http.get
+		(
+			//'http://rest-service.guides.spring.io/greeting',
+			'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Skelet0r',
+			{
+				headers:
+				{
+					'X-Riot-Token': 'RGAPI-c44697f9-243e-4784-8aa4-72c4d414d0a1',
+					//'Authorization': 'X-Riot-Token RGAPI-c44697f9-243e-4784-8aa4-72c4d414d0a1',
+					'Access-Control-Allow-Origin': '*'
+					// etc.
+				}
+			}
+		);*/
+		
+		var code = null;
+		var response = null;
+		var data = null;
+
+		$http
+		(
+			{
+				method: 'GET', 
+				//url: 'http://rest-service.guides.spring.io/greeting'
+				url: 'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Skelet0r',
+				//Authorization: 'RGAPI-c44697f9-243e-4784-8aa4-72c4d414d0a1',
+				headers:
+				{
+					'Access-Control-Allow-Origin': '*'
+					//'X-Riot-Token': 'RGAPI-c44697f9-243e-4784-8aa4-72c4d414d0a1',
+					//'Authorization': 'RGAPI-c44697f9-243e-4784-8aa4-72c4d414d0a1'
+				},
+				params:
+				{
+					'api_key': 'RGAPI-c44697f9-243e-4784-8aa4-72c4d414d0a1'
+				}
+				//Access-Control-Allow-Origin: '*'
+			}
+		).
+        then
+		(	
+			function(response)
+			{
+				status = response.status;
+				data = response.data;
+				console.log('Venga te carreo prro :v');
+				console.log(data);
+			}, 
+			function(response)
+			{
+				data = response.data || 'Request failed';
+				status = response.status;
+				
+				console.log('Esto no está funkando :c');
+				console.log(status);
+			}
+		);
+	}
 });
--- summoner.js
+++ summoner.js
@@ -48,26 +48,7 @@ component
 			
 			$scope.getSummoner2 = function()
 			{
-				return $http.get
-				(
-					'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Skelet0r', 
-					{
-						'headers':
-						{
-							'X-Riot-Token': 'RGAPI-af20bc8a-2b72-457a-bfc5-751995130c65',
-							'Origin': "https://developer.riotgames.com",
-							//'Accept-Charset': "application/x-www-form-urlencoded; charset=UTF-8",
-							'Access-Control-Allow-Origin': '*',
-							//'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
-							'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-						}
-						/*,
-						'params':
-						{
-							api_key: 'RGAPI-af20bc8a-2b72-457a-bfc5-751995130c65'
-						}*/
-					}
-				);
+				summonerService.getSummonerData2();
 			}
 			
 			$scope.getSummoner2();
